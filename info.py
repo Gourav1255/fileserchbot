abefecc273093f8de3db64b771f5c199ce4cdeeb
Gourav1255 <114558061+Gourav1255@users.noreply.github.com>
2022-12-29 00:34:59 +0530

    Update info.py

--- info.py
+++ info.py
@@ -35,7 +35,7 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://sdisk123:sdisk123@cluster0.v28p5q3.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://fileshare:fileshare@cluster0.irrapwn.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
