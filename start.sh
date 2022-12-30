62beb30551b06b8e0d860434917f684ac14d9eee
Gourav1255 <114558061+Gourav1255@users.noreply.github.com>
2022-12-30 13:15:23 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /fileserchbot
 fi
-cd /LazyPrincessV2
+cd /fileserchbot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
