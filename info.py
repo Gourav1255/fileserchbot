8a7f1589511c6638d7c78fd8fd8dfd1db30f11dc
Gourav1255 <114558061+Gourav1255@users.noreply.github.com>
2022-12-30 13:33:08 +0530

    Update info.py

--- info.py
+++ info.py
@@ -35,7 +35,7 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Sohanrazz:Sohanrazz@cluster0.cdulol1.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://clonetgbot:clonetgbot@cluster0.v8hwx1m.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Forward bot')
 
